9d45d9a45d0d11fcdab374b6d349179930a3068b
kongheng <kongheng.long18@gmail.com>
2024-09-15 09:11:43 +0700

    feat: deployment

--- order-service/src/main/java/com/kh/order/controller/HomeController.java
+++ order-service/src/main/java/com/kh/order/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.kh.order.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,16 @@ public class HomeController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${api.product.base-url}")
+    private String productBaseUrl;
+
+    @Value("${api.product.get-product}")
+    private String getProductUrl;
+
     @GetMapping
     public String greeting() {
         try {
-            String response = restTemplate.getForObject("https://localhost.sbilhbank.com.kh:9002/product", String.class);
+            String response = restTemplate.getForObject(productBaseUrl + getProductUrl, String.class);
             return "Hello from order service!" + "\n" + response;
         } catch (Exception e) {
             throw new RuntimeException(e);
